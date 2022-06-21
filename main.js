6e5044f920ad5d2cde87c13281f676f81aec63cf
Mehria Rahmani <mehriarahmani4@gmail.com>
2022-06-21 14:20:01 -0700

    add menu to header using js

---
+++ main.js
@@ -0,0 +1,22 @@
+const mobileMenu = document.querySelector('.mobile-container');
+const closeMenu = document.querySelector('.menu-close');
+const navMenu = document.querySelector('.nav-container');
+const menuBar = document.querySelector('.menu_bar');
+const menuClose = document.querySelector('.menu-close');
+const about =  document.querySelectorAll('.mobile-navlink');
+menuBar.addEventListener('click', () => {
+  mobileMenu.style.display = 'flex';
+  navMenu.style.display = 'none';
+});
+
+menuClose.addEventListener('click', () => {
+  mobileMenu.style.display ='none';
+  navMenu.style.display = 'flex';
+});
+
+about.forEach((n) => {
+  n.addEventListener('click', () => {
+    mobileMenu.style.display = 'none';
+    navMenu.style.display = 'flex';
+  });
+});
