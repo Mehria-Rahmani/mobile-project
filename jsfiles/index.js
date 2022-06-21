98acaf1cdf1506ea5fe3d0d7befd800e778c0342
Mehria Rahmani <mehriarahmani4@gmail.com>
2022-06-21 03:20:26 -0700

    add js features to proj

--- jsfiles/index.js
+++ jsfiles/index.js
@@ -1,19 +1,10 @@
-const menubtn = document.querySelector('#menu');
-function menubtnClicked() {
-  document.querySelector('.menuexpand').classList.toggle('expand');
-  document.querySelector('.menu').classList.toggle('expand');
-  document.querySelector('.name').classList.toggle('expand');
-  document.querySelector('.closebtn').classList.toggle('expand');
-  document.querySelector('.toolbar').classList.toggle('height100');
-  if (document.querySelector('.menu-desktop-container').style.overflow === 'hidden') {
-    document.querySelector('.menu-desktop-container').style.overflow = 'auto';
-  } else {
-    document.querySelector('.menu-desktop-container').style.overflow = 'hidden';
-  }
-}
-menubtn.addEventListener('click', menubtnClicked);
-const closemenu = document.querySelector('.closebtn');
-closemenu.addEventListener('click', menubtnClicked);
-document.querySelectorAll('.expitem').forEach((item) => {
-  item.addEventListener('click', menubtnClicked);
+const menu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+menu.addEventListener('click', () => {
+  mobileMenu.classList.remove('display-mobile');
+});
+
+const closeIcon = document.querySelector('header .mobile-menu .close-menu a');
+closeIcon.addEventListener('click', () => {
+  mobileMenu.classList.add('display-mobile');
 });
