bf2a56ced824e790bb888b411df5601e0408ddba
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-06-21 12:01:49 +0300

    Update index.js file

--- jsfiles/index.js
+++ jsfiles/index.js
@@ -1,23 +1,19 @@
-const menubtn= document.querySelector('#menu');
-
-
+const menubtn = document.querySelector('#menu');
 function menubtnClicked() {
   document.querySelector('.menuexpand').classList.toggle('expand');
   document.querySelector('.menu').classList.toggle('expand');
   document.querySelector('.name').classList.toggle('expand');
   document.querySelector('.closebtn').classList.toggle('expand');
   document.querySelector('.toolbar').classList.toggle('height100');
-  if(document.querySelector('.container').style.overflow == 'hidden'){
-    document.querySelector('.container').style.overflow = 'auto';
-  }else{
-    document.querySelector('.container').style.overflow = 'hidden';
+  if (document.querySelector('.menu-desktop-container').style.overflow === 'hidden') {
+    document.querySelector('.menu-desktop-container').style.overflow = 'auto';
+  } else {
+    document.querySelector('.menu-desktop-container').style.overflow = 'hidden';
   }
 }
 menubtn.addEventListener('click', menubtnClicked);
-
 const closemenu = document.querySelector('.closebtn');
 closemenu.addEventListener('click', menubtnClicked);
-
 document.querySelectorAll('.expitem').forEach((item) => {
   item.addEventListener('click', menubtnClicked);
 });
