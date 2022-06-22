504f1ae4872a506249b5c8a3f1003de4b51b7ca5
Mehria Rahmani <mehriarahmani4@gmail.com>
2022-06-22 08:44:24 -0700

    fix linter errors

--- jsfiles/card.js
+++ jsfiles/card.js
@@ -1,15 +1,4 @@
 const projects = {
-  recent: {
-    name: 'First Project',
-    desc: `first project description <br> Lorem Ipsum is simply. 
-           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-           when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.`,
-    image: '../images/Snapshoot Portfolio.png',
-    techs: ['html', 'bootstrap', 'Ruby'],
-
-    live: '',
-    source: '',
-  },
   first: {
     name: 'First Project',
     desc: `first project description <br> Lorem Ipsum is simply. 
