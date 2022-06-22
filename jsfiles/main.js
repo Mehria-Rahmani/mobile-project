ee9c1f2f9dea7f13157c10aff1b808651d6f2c21
Mehria Rahmani <mehriarahmani4@gmail.com>
2022-06-22 05:35:34 -0700

    add keylistener to pop-up menu

--- jsfiles/main.js
+++ jsfiles/main.js
@@ -1,21 +1,43 @@
-const mobileMenu = document.querySelector('.mobile-container');
-const navMenu = document.querySelector('.nav-container');
-const menuBar = document.querySelector('.menu_bar');
-const menuClose = document.querySelector('.menu-close');
-const about = document.querySelectorAll('.mobile-navlink');
-menuBar.addEventListener('click', () => {
-  mobileMenu.style.display = 'flex';
-  navMenu.style.display = 'none';
+const menubtn = document.querySelector('#menu');
+
+function menubtnClicked() {
+  document.querySelector('.menuexpand').classList.toggle('expand');
+  document.querySelector('.menu').classList.toggle('expand');
+  document.querySelector('.name').classList.toggle('expand');
+  document.querySelector('.closebtn').classList.toggle('expand');
+  document.querySelector('.toolbar').classList.toggle('height100');
+  if(document.querySelector('.container').style.overflow == 'hidden'){
+    document.querySelector('.container').style.overflow = 'auto';
+  }else{
+    document.querySelector('.container').style.overflow = 'hidden';
+  }
+}
+menubtn.addEventListener('click', menubtnClicked);
+
+const closemenu = document.querySelector('.closebtn');
+closemenu.addEventListener('click', menubtnClicked);
+
+document.querySelectorAll('.expitem').forEach((item) => {
+  item.addEventListener('click', menubtnClicked);
 });
 
-menuClose.addEventListener('click', () => {
-  mobileMenu.style.display = 'none';
-  navMenu.style.display = 'flex';
+const email = document.getElementById('email');
+const subBtn = document.getElementById('subBtn');
+
+subBtn.addEventListener('click', (event) => {
+  if (!email.validity.valid) {
+    if (email.validity.valueMissing) {
+      document.getElementById('error').innerText = 'You should provide an email address';
+    } else if (email.validity.typeMismatch) {
+      document.getElementById('error').innerText = 'Your email address is incorrect';
+    } else if (email.validity.patternMismatch) {
+      document.getElementById('error').innerText = 'Your email address should be lowercase';
+    }
+    document.getElementById('error').style.visibility = 'visible';
+    event.preventDefault();
+  }
 });
 
-about.forEach((n) => {
-  n.addEventListener('click', () => {
-    mobileMenu.style.display = 'none';
-    navMenu.style.display = 'flex';
-  });
+email.addEventListener('input', () => {
+  document.getElementById('error').style.visibility = 'hidden';
 });
