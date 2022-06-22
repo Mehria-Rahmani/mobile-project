cab64c9090395c64bfdea5237d24d5a9945b9bb0
Mehria Rahmani <mehriarahmani4@gmail.com>
2022-06-22 04:57:25 -0700

    add snapshot image

---
+++ jsfiles/card.js
@@ -0,0 +1,87 @@
+const projects = {
+    first: {
+      name: 'Professional Art Printing Data',
+      desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry´s standard'",
+      image: '',
+      techs: ['html', 'bootstrap', 'css'],
+      live: '',
+      source: '',
+      privateClasses: {
+        seeProjectBtn: 'see-projec-btn-1',
+        modalContainer: 'modal-container1',
+        modalCloseBtn: 'modal-close-btn1',
+      }
+    },
+ 
+    second: {
+        name: 'Professional Art Printing Data',
+        desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry´s standard'",
+        image: '',
+        techs: ['html', 'bootstrap', 'css'],
+        live: '',
+        source: '',
+        privateClasses: {
+          seeProjectBtn: 'see-projec-btn-1',
+          modalContainer: 'modal-container1',
+          modalCloseBtn: 'modal-close-btn1',
+        }
+      },
+
+      third: {
+        name: 'Professional Art Printing Data',
+        desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry´s standard'",
+        image: '',
+        techs: ['html', 'bootstrap', 'css'],
+        live: '',
+        source: '',
+        privateClasses: {
+          seeProjectBtn: 'see-projec-btn-1',
+          modalContainer: 'modal-container1',
+          modalCloseBtn: 'modal-close-btn1',
+        }
+      },
+
+      fourth: {
+        name: 'Professional Art Printing Data',
+        desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry´s standard'",
+        image: '',
+        techs: ['html', 'bootstrap', 'css'],
+        live: '',
+        source: '',
+        privateClasses: {
+          seeProjectBtn: 'see-projec-btn-1',
+          modalContainer: 'modal-container1',
+          modalCloseBtn: 'modal-close-btn1',
+        }
+      },
+
+      fifth: {
+        name: 'Professional Art Printing Data',
+        desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry´s standard'",
+        image: '',
+        techs: ['html', 'bootstrap', 'css'],
+        live: '',
+        source: '',
+        privateClasses: {
+          seeProjectBtn: 'see-projec-btn-1',
+          modalContainer: 'modal-container1',
+          modalCloseBtn: 'modal-close-btn1',
+        }
+      },
+
+      sixth: {
+        name: 'Professional Art Printing Data',
+        desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry´s standard'",
+        image: '',
+        techs: ['html', 'bootstrap', 'css'],
+        live: '',
+        source: '',
+        privateClasses: {
+          seeProjectBtn: 'see-projec-btn-1',
+          modalContainer: 'modal-container1',
+          modalCloseBtn: 'modal-close-btn1',
+        }
+      },
+};
+
+const workCards = document.querySelector('.workCards');
